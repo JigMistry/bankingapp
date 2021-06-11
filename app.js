c3a067c22d18ec68eb5827cdbbd649f1148f8126
Jignesh <jmistry94@gmail.com>
2021-06-11 22:47:42 +0530

    banking app

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var cors = require('cors')
-const port = 3000;
+const port = process.env.PORT || 3000;
 var app = express();
 
 var mysqlConnection = mysql.createConnection({
